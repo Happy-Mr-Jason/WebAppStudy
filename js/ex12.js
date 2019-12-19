95f9f8b3b24b5417a045c5b5a30bfc575e519927
Happy-Mr-Jason <happy.seongjin.project@gmail.com>
2019-12-19 11:12:18 +0900

    JQuery Basic

---
+++ js/ex12.js
@@ -0,0 +1,8 @@
+// $ : Determiner of jquery
+// $('selector').method(); : selector have to wrap using ''.
+// .css(property, value) : change the Value of the CSS Property to value.
+// .ready() : Specify a function to execute when the DOM(Document Object Model) is fully loaded.
+
+$(document).ready(function (){
+    $('h1').css('color','red');
+});
